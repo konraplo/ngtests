0a0363289a43c422a4bc7afe1d8121fc19a1d324
Konrad Plocharski <Konrad.plocharski.ext@siemens.com>
2021-12-17 13:04:01 +0100

    bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -13,10 +14,11 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     ServerComponent,
     ServersComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = "test";
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer()
+  {
+    this.serverCreationStatus = "Server was created! name:" + this.serverName;
+  }
+
+  onUpdateServerName(event: Event)
+  {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
