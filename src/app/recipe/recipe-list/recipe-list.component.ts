e0415c202203ed5f3b727ef306b46ef077d6c87d
Konrad Plocharski <Konrad.plocharski.ext@siemens.com>
2021-12-20 11:23:33 +0100

    component recipe and model

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [ 
+    new Recipe("A test 1", "dupa", "https://images.immediate.co.uk/production/volatile/sites/2/2019/04/Dum-Aloo-e163632.jpg?webp=true&quality=45&resize=2820%2C1198"),
+    new Recipe("A test 2", "dupa 2", "https://images.immediate.co.uk/production/volatile/sites/2/2019/04/Dum-Aloo-e163632.jpg?webp=true&quality=45&resize=2820%2C1198")
+  ];
   constructor() { }
 
   ngOnInit(): void {
