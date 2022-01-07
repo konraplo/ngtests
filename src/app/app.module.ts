01b5149ad4463b5de7a403a19a3460460eb7b637
Konrad Plocharski <Konrad.plocharski.ext@siemens.com>
2022-01-07 12:39:14 +0100

    page not found route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,20 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {    path: '', component: HomeComponent  },
+  {    path: 'not-found', component: PageNotFoundComponent  },
   {    path: 'users', component: UsersComponent, children:[
         {    path: ':id/:name', component: UserComponent  }
       ] },
   {    path: 'servers', component: ServersComponent, children: [
         {    path: ':id', component: ServerComponent  },
         {    path: ':id/edit', component: EditServerComponent  }
-      ]}
+      ]},
+  {    path: '**', redirectTo: "/not-found"  },
+
 ];
 @NgModule({
   declarations: [
@@ -31,7 +35,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
