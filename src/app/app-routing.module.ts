877ccacf1858f8861c4c88ce450f5c20499280da
Konrad Plocharski <Konrad.plocharski.ext@siemens.com>
2022-02-21 15:12:25 +0100

    config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { RecipeDetailComponent } from "./recipe/recipe-detail/recipe-detail.component";
+import { RecipeStartComponent } from "./recipe/recipe-start/recipe-start.component";
 
 import { RecipeComponent } from "./recipe/recipe.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
     {path:'', redirectTo: "/recipes", pathMatch: 'full'},
-    {path:'recipes', component: RecipeComponent},
+    {path:'recipes', component: RecipeComponent, children:[
+        {path: '', component: RecipeStartComponent},
+        {path: ':id', component: RecipeDetailComponent}
+    ]},
     {path:'shopping-list', component: ShoppingListComponent}
 ]
 @NgModule({
