10657325537b452db3b6d4b9b6d3eca46d0c4581
Konrad Plocharski <Konrad.plocharski.ext@siemens.com>
2022-07-16 19:15:30 +0200

    delete ingredients

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -51,6 +51,10 @@ export class RecipeEditComponent implements OnInit {
     }));
   }
 
+  onDeleteIngredient(index: number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   getControls() { // a getter!
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
